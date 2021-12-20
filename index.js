b27c34c4ddf17d7c8c6d2b317245c9f85057b6e6
Amish2101 <kaklotaramish@gmail.com>
2021-12-20 18:48:25 +0530

    second commit

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 var country = document.form.country;
 var state = document.form.state;
 var city = document.form.city;
-
-addEventListener('load', async () => {
-
-
-
+const api = async () => {
     var ref = await fetch("https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries%2Bstates%2Bcities.json");
     let data = await ref.json();
-
-
-
     for (let x = 0; x <= 200; x++) {
         country.options[country.options.length] = new Option(data[x].name);
 
     }
     country.onchange = function () {
         state.length = 1;
+
         let val = document.form.country.value;
         console.log(val);
+
+
         for (let y = 0; y <= 200; y++) {
             if (val == data[y].name) {
-                for (let z = 0; z <= 200; z++) {
-                    console.log(data[y].states[z].name);
-                    state.options[state.options.length] = new Option(data[y].states[z].name);
-
+                for (let z = 0; z <= data[y].states.length; z++) {
+                    try {
+                        state.options[state.options.length] = new Option(data[y].states[z].name);
+                    } catch (e) {
+                        console.log(e);
+                    }
                 }
             }
         }
     }
+
 
     state.onchange = function () {
         city.length = 1;
@@ -38,13 +37,17 @@ addEventListener('load', async () => {
         for (let i = 0; i <= 200; i++) {
             if (valofcountry == data[i].name) {
                 console.log(data[i].name);
-                for (let j = 0; j <= 200; j++) {
-                    if (valofstate == data[i].states[j].name) {
-                        console.log(data[i].states[j].name);
-                        for (let k = 0; k <= 200; k++) {
-                            console.log(data[i].states[j].cities[k].name);
-                            city.options[city.options.length] = new Option(data[i].states[j].cities[k].name);
+                for (let j = 0; j <= data[i].states.length; j++) {
+                    try {
+                        if (valofstate == data[i].states[j].name) {
+                            console.log(data[i].states[j].name);
+                            for (let k = 0; k <= data[i].states[j].cities.length; k++) {
+                                console.log(data[i].states[j].cities[k].name);
+                                city.options[city.options.length] = new Option(data[i].states[j].cities[k].name);
+                            }
                         }
+                    } catch (e) {
+                        console.log(e);
                     }
                 }
 
@@ -53,6 +56,17 @@ addEventListener('load', async () => {
         }
 
     }
+
+
+}
+
+
+addEventListener('load', () => {
+
+
+
+    api();
+
 
 
 
@@ -69,6 +83,17 @@ const showAllData = [
         "email": "amish.taglin@gmail.com",
         "gender": "male",
         "age": "2002-01-21",
+        "hobbies": "Reading",
+        "country": "Australia",
+        "state": "Tasmania",
+        "city": "Beaconsfield",
+        "date": new Date()
+    },
+    {
+        "name": "himanshu",
+        "email": "himanshu.taglin@gmail.com",
+        "gender": "male",
+        "age": "2001-07-21",
         "hobbies": "Reading",
         "country": "India",
         "state": "Gujarat",
@@ -126,9 +151,11 @@ function cleanForm() {
 }
 
 var seleteRow = null;
+
 function saveData() {
     var data = getData();
     insetData(data);
+
 }
 
 function getData() {
@@ -149,7 +176,7 @@ function getData() {
     obj2["age"] = document.form.age.value;
     console.log(obj2["age"].value);
     let marked = document.form.hobbies;
-    let hobbiArry = [];
+    var hobbiArry = [];
     for (let checkbox of marked) {
         if (checkbox.checked) {
             console.log(checkbox.value);
@@ -157,7 +184,12 @@ function getData() {
         }
     }
     obj2["hobbies"] = hobbiArry;
+
+
+
     obj2["country"] = document.form.country.value;
+
+
     obj2["state"] = document.form.state.value;
     obj2["city"] = document.form.city.value;
 
@@ -186,91 +218,178 @@ function insetData(data) {
 
 }
 
+var buttonCheck = false;
+
 function edit(data) {
-    document.form.add.style.visibility = "hidden";
-    var update1 = document.createElement("BUTTON");
-    let btnname1 = document.createTextNode("Update");
-    update1.appendChild(btnname1);
-    document.getElementById("form").after(update1);
-    var cancel1 = document.createElement("BUTTON");
-    let btnname2 = document.createTextNode("Cancel");
-    cancel1.appendChild(btnname2);
-    document.getElementById("form").after(cancel1);
-    cleanForm();
+    if (buttonCheck == false) {
+
+        document.form.add.style.visibility = "hidden";
+        var update1 = document.createElement("BUTTON");
+        let btnname1 = document.createTextNode("Update");
+        update1.appendChild(btnname1);
+        document.getElementById("form").after(update1);
+        var cancel1 = document.createElement("BUTTON");
+        let btnname2 = document.createTextNode("Cancel");
+        cancel1.appendChild(btnname2);
+        document.getElementById("form").after(cancel1);
+        buttonCheck = true;
+    }
+
+
     var seleteRow = data.parentElement.parentElement;
+    let ind = seleteRow.rowIndex - 1;
     document.form.name.value = seleteRow.cells[0].innerHTML;
     document.form.email.value = seleteRow.cells[1].innerHTML;
     document.form.gender.value = seleteRow.cells[2].innerHTML;
-    document.form.age.value = seleteRow.cells[3].innerHTML;
-    document.form.hobbies.value = seleteRow.cells[4].innerHTML;
-    document.form.country.value = seleteRow.cells[5].innerHTML;
-    document.form.state.value = seleteRow.cells[6].innerHTML;
-    document.form.city.value = seleteRow.cells[7].innerHTML;
-
-
-    if (vaditionForm()) {
-
-        console.log("error");
-
-
-
+    let final = showAllData[ind].age;
+    document.form.age.value = final;
+    let dataofhobbie = seleteRow.cells[4].innerHTML;
+    console.log(dataofhobbie);
+    console.log(dataofhobbie.indexOf("Reading"));
+    console.log(showAllData[dataofhobbie.indexOf("Reading")].hobbies);
+    if (dataofhobbie.match(/Reading/ig)) {
+        document.form.hobbies[0].checked = true;
     } else {
-        vaditionForm();
-        update1.onclick = function () {
-            console.log(update1);
-            let ind = seleteRow.rowIndex - 1;
-            let n = document.form.name.value;
-            let e = document.form.email.value;
-            let g = document.form.gender.value;
-            let a = document.form.age.value;
-            let marked = document.form.hobbies;
-            let hobbiArry = [];
-            for (let checkbox of marked) {
-                if (checkbox.checked) {
-                    hobbiArry.push(checkbox.value);
-                }
+        document.form.hobbies[0].checked = false;
+    }
+    if (dataofhobbie.match(/Sport/ig)) {
+        document.form.hobbies[1].checked = true;
+    } else {
+        document.form.hobbies[1].checked = false;
+    }
+    if (dataofhobbie.match(/Traveling/ig)) {
+        document.form.hobbies[2].checked = true;
+    } else {
+        document.form.hobbies[2].checked = false;
+    }
+
+
+
+
+
+
+
+    // document.form.hobbies.value = seleteRow.cells[4].innerHTML;
+    let getCountryIndex = seleteRow.cells[5].innerHTML;
+    let getStateIndex = seleteRow.cells[6].innerHTML;
+    let getCityIndex = seleteRow.cells[7].innerHTML;
+
+
+    var getIndexData = async () => {
+
+        var ref = await fetch("https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries%2Bstates%2Bcities.json");
+        let dataofdropdown = await ref.json();
+
+        var a = 0;
+        var b = 0;
+        var c = 0;
+        for (let i = 0; i <= 200; i++) {
+            if (dataofdropdown[i].name == getCountryIndex) {
+                a = i;
+                break;
             }
-            let h = hobbiArry;
-            let c = document.form.country.value;
-            let s = document.form.state.value;
-            let ci = document.form.city.value;
-            let d = new Date();
-            console.log(showAllData[ind].name);
-            showAllData[ind].name = n;
-            showAllData[ind].email = e;
-            showAllData[ind].gender = g;
-            showAllData[ind].age = a;
-            showAllData[ind].hobbies = h;
-            showAllData[ind].country = c;
-            showAllData[ind].state = s;
-            showAllData[ind].city = ci;
-            console.log(showAllData);
-            cleanForm();
-            deleleTableData();
-            showDataFunction();
-            console.log(table);
-            document.form.add.style.visibility = "visible";
-            update1.style.visibility = "hidden";
-            cancel1.style.visibility = "hidden";
-
         }
+
+
+        for (let i = 0; i <= dataofdropdown[a].states.length; i++) {
+            if (dataofdropdown[a].states[i].name == getStateIndex) {
+                b = i;
+                break;
+            }
+        }
+
+        for (let i = 0; i <= dataofdropdown[a].states[b].cities.length; i++) {
+            if (dataofdropdown[a].states[b].cities[i].name == getCityIndex) {
+                c = i;
+                break
+            }
+        }
+
+        console.log(a + " " + b + " " + c);
+
+        document.getElementsByTagName('select')[0].selectedIndex = a + 1;
+        country.onchange();
+        document.getElementsByTagName('select')[1].selectedIndex = b + 1;
+        state.onchange();
+        document.getElementsByTagName('select')[2].selectedIndex = c + 1;
+
+
+
+
     }
 
+    getIndexData();
 
-    function deleleTableData() {
-        let table = document.getElementById("table");
-        var rowCount = table.rows.length;
-        for (var i = rowCount - 1; i > 0; i--) {
-            table.deleteRow(i);
+    
+
+
+
+
+    update1.addEventListener("click", () => {
+
+        vaditionForm();
+        console.log(update1);
+        let ind = seleteRow.rowIndex - 1;
+        let name = document.form.name.value;
+        let email = document.form.email.value;
+        let gender = document.form.gender.value;
+        let age = document.form.age.value;
+        let marked = document.form.hobbies;
+        let hobbiArry = [];
+        for (let checkbox of marked) {
+            if (checkbox.checked) {
+                hobbiArry.push(checkbox.value);
+            }
         }
+        let hobbies = hobbiArry;
+        let country = document.form.country.value;
+        let state = document.form.state.value;
+        let city = document.form.city.value;
+        let date = new Date();
+        console.log(showAllData[ind].name);
+        showAllData[ind].name = name;
+        showAllData[ind].email = email;
+        showAllData[ind].gender = gender;
+        showAllData[ind].age = age;
+        showAllData[ind].hobbies = hobbies;
+        showAllData[ind].country = country;
+        showAllData[ind].state = state;
+        showAllData[ind].city = city;
+        console.log(showAllData);
+        cleanForm();
+        deleleTableData();
+        showDataFunction();
+        console.log(table);
+        document.form.add.style.visibility = "visible";
+        update1.style.visibility = "hidden";
+        cancel1.style.visibility = "hidden";
+    buttonCheck = false;
     }
+    );
 
-    cancel1.onclick = function () {
+    cancel1.addEventListener("click", () => {
+
         document.form.reset();
+        document.form.add.style.visibility = "visible";
 
-    }
+
+    })
 
 }
+
+
+function deleleTableData() {
+    let table = document.getElementById("table");
+    var rowCount = table.rows.length;
+    for (var i = rowCount - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+}
+
+
+
+
+
 
 
 
@@ -299,6 +418,7 @@ function vaditionForm() {
     let massGender = document.form.gender[1].nextSibling;
     console.log(massGender);
     let massAge = document.form.age.nextSibling;
+    console.log(massAge);
     let massHobbies = document.form.hobbies[2].nextSibling;
     console.log(massHobbies);
     let massCountry = document.form.country.nextSibling;
@@ -309,21 +429,26 @@ function vaditionForm() {
     let gender = document.form.gender;
     console.log(gender);
     let age = document.form.age;
+    console.log(age.value);
     let hobbies = document.form.hobbies;
     let country = document.form.country;
     let state = document.form.state;
     let city = document.form.city;
 
-    if (name.value.length <= 0 || name.value == 0) {
+
+
+
+    var error = true;
+    if (name.value.length == 0) {
         massName.innerHTML = "*Please fill name";
-        return false;
+        error = false;
     } else {
         massName.innerHTML = "";
     }
 
     if (email.value.length == 0) {
         massEmail.innerHTML = "*Please fill email data";
-        return false;
+        error = false;
     } else {
         massEmail.innerHTML = "";
     }
@@ -332,7 +457,7 @@ function vaditionForm() {
 
     if (!(email.value.match(emailvalistring))) {
         massEmail.innerHTML = "*Please Enter valid Email Address";
-        return false;
+        error = false;
     } else {
         massEmail.innerHTML = "";
     }
@@ -340,37 +465,22 @@ function vaditionForm() {
     console.log(gender[0]);
     if (gender[0].checked == false && gender[1].checked == false) {
         massGender.innerHTML = "*Please Select Checkbox";
-        return false;
+        error = false;
     } else {
         massGender.innerHTML = "";
     }
 
 
-    if (age.value.length == 0) {
+    if (age.value == "") {
         massAge.innerHTML = "*Please fill DoB ";
-        return false;
-    } else {
-        massAge.innerHTML = "";
-    }
-    let v = age.value;
-    console.log(v);
-    let valueinstring = v.toString();
-    let cuttingvalue = valueinstring.slice(0, 4);
-    let date = new Date;
-    let todayYear = date.getFullYear();
-    console.log(todayYear);
-    let a = parseInt(todayYear);
-    let b = parseInt(cuttingvalue);
-    if (b > a) {
-        massAge.innerHTML = "*you choose wrong year";
-        return false;
+        error = false;
     } else {
         massAge.innerHTML = "";
     }
 
     if (hobbies[0].checked == false || hobbies[1].checked == false && hobbies[2] == false) {
         massHobbies.innerHTML = "*Please select box";
-        return false;
+        error = false;
     }
     else {
         massHobbies.innerHTML = "";
@@ -379,27 +489,29 @@ function vaditionForm() {
 
     if (country.value == "--Select Country--") {
         massCountry.innerHTML = "*Please select country on list";
-        return false;
+        error = false;
     } else {
         massCountry.innerHTML = "";
     }
 
     if (state.value == "--Select State--") {
         massState.innerHTML = "*Please select state on list";
-        return false;
+        error = false;
     } else {
         massState.innerHTML = "";
     }
 
     if (city.value == "--Select City--") {
-        massCity.innerHTML = "*Please fill name";
-        return false;
+        massCity.innerHTML = "*Please select city on list";
+        error = false;
     }
     else {
         massCity.innerHTML = "";
     }
+    return error;
 
 }
+
 
 function deleleTableData() {
     let table = document.getElementById("table");
@@ -417,18 +529,18 @@ function search() {
     const arr = [];
     var index = 0;
 
-    const final = () =>{
+    const final = () => {
         let value = val.toLowerCase();
         console.log(value);
         let data1 = value.toLowerCase();
         console.log(data1);
-        showAllData.findIndex((v,i) => {
+        showAllData.findIndex((v, i) => {
             console.log("data" + v);
-            console.log("index of array" +  i);
+            console.log("index of array" + i);
             let data2 = v.name.toLowerCase();
-            let com =  data2.indexOf(data1);
+            let com = data2.indexOf(data1);
 
-            if(com > -1){
+            if (com > -1) {
 
                 arr.push(showAllData[i]);
                 console.log(arr[index]);
@@ -443,7 +555,7 @@ function search() {
 
 
         })
-        showAllData.splice(0,showAllData.length, ...arr);
+        showAllData.splice(0, showAllData.length, ...arr);
         deleleTableData();
         showDataFunction();
         console.log(showAllData);
@@ -453,6 +565,12 @@ function search() {
 
 
     final();
+
+}
+
+function reset(){
+    console.log("hellomoto");
+    document.querySelector("input[type='search']").innerHTML = '';
 
 }
 
